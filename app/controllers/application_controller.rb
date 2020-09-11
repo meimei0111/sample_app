3101f2a6a72e2628373c7f74852b514b587d2c02
meimei <mhui0111@gmail.com>
2020-09-11 17:41:42 +0800

    Finish chapter 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :switch_locale
+
+  def hello
+    render html: "hello,world!"
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
+  private
+
+  def switch_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')] # it will load all file at locales folder and, only read rm anf yml file for i18n
+
+    config.i18n.default_locale = :en # language default project
+    I18n.available_locales = [:en] # define all laguage that you project allow
   end
 end
