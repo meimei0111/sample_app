0df45b2ae53f16b195ff61efe3add5cf758cb9ad
meimei <mhui0111@gmail.com>
2020-09-25 00:21:59 +0800

    chapter8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
+      log_in @user
       flash[:success] = t ".success_mess_create"
       redirect_to @user
     else
