f262ec5ae01dc6c4b7942a607798646cd4554d3e
meimei <mhui0111@gmail.com>
2020-09-29 17:16:50 +0800

    chapter11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      params[:session][:remember_me].eql? Settings.collections.session_controller_create ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me].eql? Settings.collections.session_controller_create ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        flash[:warning] = t ".sessions.create.warn_mess_session"
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = t "sessions.create.fail_mess_session"
       render :new
