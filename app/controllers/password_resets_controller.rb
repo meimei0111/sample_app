a27e3e14dd4317a591b2850505f655bde3dcefce
meimei <mhui0111@gmail.com>
2020-10-02 01:34:29 +0800

    chapter12

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,64 @@
+class PasswordResetsController < ApplicationController
+  before_action :load_user, only: %i(edit update)
+  before_action :valid_user, :check_expiration, only: %i(edit update)
+
+  def new; end
+
+  def create
+    @user = User.find_by email: params[:password_reset][:email].downcase
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t ".success_acct_sent"
+      redirect_to root_path
+    else
+      flash.now[:danger] = t ".fail_acct_sent"
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add :password, "can't be empty"
+      render :edit
+    elsif @user.update user_params
+      log_in @user
+      @user.update_attribute :reset_digest, nil
+      flash[:success] = t ".success_psword_update"
+      redirect_to @user
+    else
+      flash.now[:danger] = t ".fail_ps_reset_mess"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :password, :password_confirmation
+  end
+
+  def load_user
+    @user = User.find_by email: params[:email]
+    return if @user
+    
+    flash[:warning] = "user not found"
+    redirect_to root_path
+  end
+
+  def valid_user
+    return if @user.activated? && @user.authenticated?(:reset, params[:id])
+
+    flash[:danger] = t ".fail_mess_ps_reset"
+    redirect_to root_path
+  end
+
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = t ".ps_expired_mess"
+    redirect_to new_password_reset_path
+  end
+end
